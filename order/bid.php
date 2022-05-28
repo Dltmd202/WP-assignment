626d7566b88a0d141f9cc2ae46e366e08e708f2e
Dltmd202 <dltmd202@gmail.com>
2022-05-28 20:17:04 +0900

    Feat: Add Bid logic

--- order/bid.php
+++ order/bid.php
@@ -25,7 +25,15 @@
   $con = mysqli_connect("localhost", "root", "1234", "wp")
   or die("Can't access DB");
 
-  $query = "SELECT sh.id as id, sh.photo as photo, sh.name as name, b.name as brand, os.id as order_id
+  $query = "SELECT sh.id as id, sh.photo as photo, sh.name as name, b.name as brand, os.id as order_id,
+                   (
+                      SELECT s.price
+                          FROM sell as s
+                          LEFT JOIN order_shoe as os on s.order_shoe_id = os.id
+                          WHERE os.size = {$size} && os.id = {$id}
+                          ORDER BY s.price DESC
+                          LIMIT 1
+                   ) as direct_purcharse_price
                      FROM shoe as sh
                      LEFT JOIN brand as b on sh.brand_id = b.id
                      LEFT JOIN order_shoe as os on os.shoe_id = {$id}
@@ -44,18 +52,58 @@
     <?= $row['name']?>
   </div>
   <form action="./bid_action.php?id=<?=$id?>&size=<?=$size?>&order_id=<?=$row['order_id']?>" method="post">
-    <label>
-      구매 희망가
-      <input type="number" name="price">
-    </label>
-    <label>
-      입찰 마감기한
-      <input type="number" name="period">
-    </label>
-    <input type="submit" value="submit">
+    <div>
+      <label>
+        <input type="checkbox" name="immediate"
+               value="immediate" id="immediate_check" onchange="isImmediatelyPurchaseChecked(this)">
+        <span id="min_price"><?=$row['direct_purcharse_price']?></span>
+          원에 즉시 구매하기
+      </label>
+    </div>
+    <div>
+      <label>
+        구매 희망가
+        <input type="number" name="price" id="price" onchange="priceChanged(<?=$row['direct_purcharse_price']?>)">
+      </label>
+      <label>
+        입찰 마감기한
+        <input type="number" name="period" id="period">
+      </label>
+      <input type="submit" value="submit">
+    </div>
   </form>
 </div>
 
 <?php include '../footer.php'; ?>
+<script>
+    function isImmediatelyPurchaseChecked(checked){
+        if(checked.checked){
+            document.getElementById('period').disabled = true;
+            document.getElementById('price').disabled = true;
+        } else {
+            document.getElementById('period').disabled = false;
+            document.getElementById('price').disabled = false;
+        }
+    }
+
+    function isImmediatelyPurchaseCheckedVal(val){
+        const checked = document.getElementById('immediate_check');
+        checked.checked = true;
+        isImmediatelyPurchaseChecked(checked);
+    }
+
+    function priceChanged(min_price){
+        const price = document.getElementById('price');
+        const period = document.getElementById('period');
+        if(parseInt(price.value) > parseInt(min_price)){
+            price.value = min_price;
+        }
+        if(parseInt(price.value) == parseInt(min_price)){
+            price.value = "";
+            period.value = "";
+            isImmediatelyPurchaseCheckedVal()
+        }
+    }
+</script>
 </body>
 </html>
--- order/bid_action.php
+++ order/bid_action.php
@@ -3,42 +3,149 @@
 $mysqli = new mysqli("localhost", "root", "1234", "wp");
 $mysqli->begin_transaction();
 
-try{
-  $price = $_POST['price'];
-  $period = $_POST['period'];
-  $order_id = $_GET['order_id'];
-  $id = $_GET['id'];
-  $size = $_GET['size'];
-  $user_id = $_SESSION['user_id'];
+if($_POST['immediate']){
+  try{
+    $id = $_GET['id'];
+    $size = $_GET['size'];
+    $user_id = $_SESSION['user_id'];
 
-  $query = "select u.money as money from user as u where u.id = ?";
-  $stmt = $mysqli->prepare($query);
-  $stmt->bind_param("i", $user_id);
-  $stmt->execute();
-  $stmt->bind_result($money);
-  $stmt->fetch();
-  $stmt->close();
+    $query = "SELECT s.id, s.price, s.order_shoe_id
+                FROM sell as s
+                LEFT JOIN order_shoe as os on s.order_shoe_id = os.id
+                WHERE os.size = ? && os.shoe_id = ? && s.is_sold = 0 && now() <= s.deadline
+                ORDER BY s.price DESC
+                LIMIT 1";
 
-  if($money < $price) throw new InvalidArgumentException("금액이 부족합니다.");
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("ii", $size, $id);
+    $stmt->execute();
+    $stmt->bind_result($sell_id, $price, $order_shoe_id);
+    $stmt->fetch();
+    $stmt->close();
+
+    $query = "select u.money as money from user as u where u.id = ?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    $stmt->bind_result($money);
+    $stmt->fetch();
+    $stmt->close();
 
 
-  $query = "insert into bid(price, user_id, order_shoe_id, date, deadline) 
+    if($money < $price) {
+      echo $price;
+      echo "<br>";
+      echo $money;
+      throw new InvalidArgumentException("금액이 부족합니다.");
+    }
+
+    $query = "update sell as s set s.is_sold = 1 WHERE s.id = ?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("i",$sell_id);
+    $stmt->execute();
+    $stmt->close();
+
+    $query = "insert into bid(price, user_id, is_success, order_shoe_id, date, deadline) 
+             values(?, ?, 1, ?, now(), DATE_ADD(now(), INTERVAL 1 DAY))";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("iii", $price, $user_id, $order_shoe_id);
+    $stmt->execute();
+    $stmt->close();
+
+    $mysqli->commit();
+
+    echo "
+  <script>
+    alert('{$price} 원으로 구매되었습니다.');
+    location.href='..';
+    </script>
+  ";
+
+
+  }catch (mysqli_sql_exception $exception){
+    $mysqli->rollback();
+    throw $exception;
+    echo "
+    <script>
+      alert('서버 상태가 좋지않습니다.');
+      location.href='..';
+      </script>
+    ";
+  } catch (InvalidArgumentException $exception){
+    $mysqli->rollback();
+    echo "
+  <script>
+    alert('금액이 부족합니다.');
+//    location.href='..';
+    </script>
+  ";
+  }
+} else {
+
+  try {
+    $price = $_POST['price'];
+    $period = $_POST['period'];
+    $order_id = $_GET['order_id'];
+    $id = $_GET['id'];
+    $size = $_GET['size'];
+    $user_id = $_SESSION['user_id'];
+
+    $query = "select u.money as money from user as u where u.id = ?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("i", $user_id);
+    $stmt->execute();
+    $stmt->bind_result($money);
+    $stmt->fetch();
+    $stmt->close();
+
+    if ($money < $price) throw new InvalidArgumentException("금액이 부족합니다.");
+
+    $query = "SELECT s.id, s.price, s.order_shoe_id
+                FROM sell as s
+                LEFT JOIN order_shoe as os on s.order_shoe_id = os.id
+                WHERE os.size = ? && os.shoe_id = ? && s.is_sold = 0 && now() <= s.deadline
+                ORDER BY s.price DESC
+                LIMIT 1";
+
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("ii", $size, $id);
+    $stmt->execute();
+    $stmt->bind_result($sell_id, $min_price, $order_shoe_id);
+    $stmt->fetch();
+    $stmt->close();
+
+    if($min_price <= $price){
+      $query = "update sell as s set s.is_sold = 1 WHERE s.id = ?";
+      $stmt = $mysqli->prepare($query);
+      $stmt->bind_param("i",$sell_id);
+      $stmt->execute();
+      $stmt->close();
+
+      $query = "insert into bid(price, user_id, is_success, order_shoe_id, date, deadline) 
+             values(?, ?, 1, ?, now(), DATE_ADD(now(), INTERVAL 1 DAY))";
+      $stmt = $mysqli->prepare($query);
+      $stmt->bind_param("iii", $price, $user_id, $order_shoe_id);
+      $stmt->execute();
+      $stmt->close();
+    } else {
+      $query = "insert into bid(price, user_id, order_shoe_id, date, deadline) 
              values(?, ?, ?, now(), DATE_ADD(now(), INTERVAL ? DAY))";
-  $stmt = $mysqli->prepare($query);
-  $stmt->bind_param("iiii", $price, $user_id, $order_id, $period);
-  $stmt->execute();
-  $stmt->close();
+      $stmt = $mysqli->prepare($query);
+      $stmt->bind_param("iiii", $price, $user_id, $order_id, $period);
+      $stmt->execute();
+      $stmt->close();
 
-  $query = "update user as u set u.money = u.money - ? WHERE u.id = ?";
-  $stmt = $mysqli->prepare($query);
-  $stmt->bind_param("ii", $price, $user_id);
-  $stmt->execute();
-  $stmt->close();
+      $query = "update user as u set u.money = u.money - ? WHERE u.id = ?";
+      $stmt = $mysqli->prepare($query);
+      $stmt->bind_param("ii", $price, $user_id);
+      $stmt->execute();
+      $stmt->close();
+    }
 
 
-  $mysqli->commit();
-
-  echo "
+    $mysqli->commit();
+    $price = min($price, $min_price);
+    echo "
   <script>
     alert('{$price} 원으로 입찰이 성사되었습니다.');
     location.href='..';
@@ -46,20 +153,21 @@ try{
   ";
 
 
-} catch (mysqli_sql_exception $exception){
-  $mysqli->rollback();
-  echo "
+  } catch (mysqli_sql_exception $exception) {
+    $mysqli->rollback();
+    echo "
   <script>
     alert('서버 상태가 좋지않습니다.');
     location.href='..';
     </script>
   ";
-} catch (InvalidArgumentException $exception){
-  $mysqli->rollback();
-  echo "
+  } catch (InvalidArgumentException $exception) {
+    $mysqli->rollback();
+    echo "
   <script>
     alert('금액이 부족합니다.');
     location.href='..';
     </script>
   ";
+  }
 }
