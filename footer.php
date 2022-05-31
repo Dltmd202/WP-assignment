4524ab40fa543ef4fbd7c86f84e80480ed5ee900
Dltmd202 <dltmd202@gmail.com>
2022-05-31 13:40:09 +0900

    Feat: Add footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,51 @@
-<?php
+<div class="container">
+  <footer>
+    <div class = "inner">
+        <div class = "service_area">
+            <div class = "footer_menu">
+                    <div class = "menu_box">
+                        <ul class = "menu_list">
+                            <li><a href=""><strong>이용안내</strong></a></li>
+                            <li><a href="">검수기준</a></li>
+                            <li><a href="">이용정책</a></li>
+                            <li><a href="">페널티정책</a></li>
+                            <li><a href="">커뮤니티 가이드라인</a></li>
+                        </ul>
+                        <ul class = "menu_info">
+                            <li><a href=""><strong>고객지원</strong></a></li>
+                            <li><a href="">공지사항</a></li>
+                            <li><a href="">서비스 소개</a></li>
+                            <li><a href="">쇼룸 안내</a></li>
+                            <li><a href="">판매자 방문접수</a></li>
+                        </ul>
+                    </div>
+            </div>
+            <div class = "customer_service">
+                <ul class = "customer_list">
+                        <li><a href=""><strong>고객센터 1588-1588</strong></a></li>
+                        <li>운영시간 평일 11:00 - 18:00</li>
+                        <li>점심시간 13:00 - 14:00</li>
+                        <li>문의하기는 앱에서만 가능합니다.</li>
+                    </ul>
+            </div>
+        </div>
+        <div class = "info_bottom">
+            <div class = "corperate_info">
+                <ul>
+                    <li><a href="">회사소개</a></li>
+                    <li><a href="">인재채용</a></li>
+                    <li><a href="">이용약관</a></li>
+                    <li><a href="">개인정보처리방침</a></li>
+                </ul>
+            </div>
+            <div class = "address">
+                <ul>
+                  <li>슈크림 주식회사(주)</li>
+                  <li>사업자 등록번호: 123-123-123</li>
+                  <li>주소: 충청남도 천안시 병천면 충절로 1600</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  </footer>
+</div>
--- order/bid.php
+++ order/bid.php
@@ -4,6 +4,7 @@
   <link href="/css/header.css" rel="stylesheet">
   <link href="/css/common.css" rel="stylesheet">
   <link href="/css/index.css" rel="stylesheet">
+  <link href="/css/footer.css" rel="stylesheet">
 </head>
 <body>
 
